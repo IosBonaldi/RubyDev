91195bad810c56072267f5edec5333a276be1070
IosBonaldi <iosbnaldi@gmail.com>
2019-01-25 15:29:08 -0200

    Added nav bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,11 +5,14 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "Blogs | Devcamp Portfolio"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
+    @page_title = @blog.title+" | Devcamp Portfolio"
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
